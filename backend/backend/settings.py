49eedaf50fcb2c599c67a04820bb09e5cf0c88dd
BringasE91 <edgarbringasmacalupu@gmail.com>
2025-08-01 19:35:58 -0500

    agregada url permitida intento 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://gastos-personales-lz6i.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['gastos-personales-lz6i.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
